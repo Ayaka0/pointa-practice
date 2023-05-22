cc89a6c294af4756c5a2ef9603df5bb1b01e04dd
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-05-22 21:57:25 +0900

    Add files via upload

---
+++ point-list1-5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int hoge;
+	int *hoge_p;
+
+	/*hoge_pにhogeへのポインタを設定*/
+	hoge_p = &hoge;
+
+	/*hoge_pの値を表示*/
+	printf("hoge_p..%p\n", (void*)hoge_p);
+
+	/*hoge_pに１加算*/
+	hoge_p++;
+
+	/*hoge_pの値を表示*/
+	printf("hoge_p..%p\n", (void*)hoge_p);
+
+	/*hoge_pに３加算した値を表示*/
+	printf("hoge_p..%p\n", (void*)(hoge_p + 3));
+
+	return 0;
+}
