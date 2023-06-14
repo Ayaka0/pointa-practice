9c65e04f19be4dfece4ac806fc6c1bc9c237cc5f
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-14 22:38:06 +0900

    Add files via upload

--- pointa10-1.c
+++ pointa10-1.c
@@ -8,26 +8,37 @@ void swap(int *x, int *y)
 	*y = temp;
 }
 
-//*n1 <= *n2となるようにソート
-void sort2(int *n1, int *n2)
+/*n1 <= *n2となるようにソート
+void sort2(int *n1, int *n2, int n3)
 {
 	if (*n1 > *n2)
 		swap(n1, n2);
 }
+*/
+
+void sort3(int* n1, int* n2, int* n3)
+{
+	if (*n1 > *n2) swap(n1, n2);
+	if (*n2 > *n3) swap(n2, n3);
+	if (*n1 > *n2) swap(n1, n2);
+}
+
 
 int main(void)
 {
-	int a, b;
+	int a, b, c;
 
 	puts("2つの整数を入力せよ");
 	printf("整数A："); scanf("%d", &a);
 	printf("整数B："); scanf("%d", &b);
+	printf("整数C："); scanf("%d", &c);
 
-	sort2(&a, &b);
+	sort3(&a, &b, &c);
 
 	puts("昇順にソートしました。");
 	printf("整数Aは%dです。\n", a);
 	printf("整数Bは%dです。\n", b);
+	printf("整数Cは%dです。\n", c);
 
 	return 0;
 
