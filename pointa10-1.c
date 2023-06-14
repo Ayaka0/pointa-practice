0e7a8ac049b8583cb03d5c1413fb0643768f1c63
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-14 21:56:51 +0900

    Add files via upload

--- pointa10-1.c
+++ pointa10-1.c
@@ -1,75 +1,34 @@
 #include<stdio.h>
 
-//y年は閏年か
-int is_leap(int y)
+//xとyが指すオブジェクトの値を交換
+void swap(int *x, int *y)
 {
-	return y % 4 == 0 && y % 100 != 0 || y % 400 = 0;
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
-//y年m月の日数
-int days_of_month(int y, int m)
+//*n1 <= *n2となるようにソート
+void sort2(int *n1, int *n2)
 {
-	int mdays[][12] = {
-		{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, //平年
-		{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, //閏年
-	};
-	return mdays[is_leap(y)][m - 1];
+	if (*n1 > *n2)
+		swap(n1, n2);
 }
-
-//*y年*m月*d日を前日の日付に更新
-void decrement_date(int* y, int* m, int* d) 
-{
-	if (*d > 1)
-		--* d;
-	else {
-		if (-- * m < 1) {
-			--* y;
-			*m = 12;
-		}
-		*d = days_of_month(*y, *m);
-	}
-}
-
-//*y年*m月*d日を翌日の日付に更新
-void increment_data(int* y, int* m, int* d)
-{
-	if (*d < days_of_month(*y, *m))
-		++* d;
-	else {
-		if (++ * m > 12) {
-			++* y;
-			*m = 1;
-		}
-		*d = 1;
-	}
-}
-
 
 int main(void)
 {
-	int n;
-	int y, m, d;
+	int a, b;
 
-	puts("日付を入力せよ");
-	printf("年："); scanf("%d", &y);
-	printf("月："); scanf("%d", &m);
-	printf("日："); scanf("%d", &d);
+	puts("2つの整数を入力せよ");
+	printf("整数A："); scanf("%d", &a);
+	printf("整数B："); scanf("%d", &b);
 
-	printf("何日戻しますか："); scanf("%d", &n);
+	sort2(&a, &b);
 
-
-	int i;
-	for (i = 0; i < n; i++)
-		decrement_date(&y, &m, &d);
-
-	printf("%d年%d月%d日になりました。\n", y, m, d);
-
-	printf("何日進めますか："); scanf("%d", &n);
-	for (i = 0; i < n; i++)
-		increment_date(&y, &m, &d);
-
-	printf("%d年&d月%d日になりました。\n", y, m, d);
-
+	puts("昇順にソートしました。");
+	printf("整数Aは%dです。\n", a);
+	printf("整数Bは%dです。\n", b);
 
 	return 0;
+
 }
