a4a770793750e474e0924fb9ca7771d7b05959a4
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-18 19:58:13 +0900

    Add files via upload

--- pointa10-1.c
+++ pointa10-1.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
 
+void set_idx(int* v, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		v[i] = i;
+}
+
+
 int main(void)
 {
-	int a[5] = { 1,2,3,4,5 };
-	int* p = a; //p‚Ía[0]‚ðŽw‚·
-	
+	int a[5];
+
+	set_idx(a, 5);
+
 	int i;
 	for (i = 0; i < 5; i++)
-		printf("a[%d] = %d  *(a+%d) = %d  p[%d] = %d *(a+%d) = %d\n", i, a[i], i, *(a + i), i, p[i], i, *(p + i));
+		printf("a[%d] = %d\n", i, a[i]);
 
-	for (i = 0; i < 5; i++)
-		printf("& a[%d] = %p a+%d = %p, &p[%d] = %p p+%d = %p\n", i, &a[i], i, (a + i), i, &p[i], i, (p + i));
 
 	return 0;
-
 }
