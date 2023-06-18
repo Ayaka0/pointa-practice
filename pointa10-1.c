03c5fcdeab121e2c09fa9934353e6cfb6ee59d8a
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-18 14:58:12 +0900

    Add files via upload

--- pointa10-1.c
+++ pointa10-1.c
@@ -2,12 +2,15 @@
 
 int main(void)
 {
-	int a[5] = { 1, 2, 3, 4, 5 };
+	int a[5] = { 1,2,3,4,5 };
 	int* p = a; //p‚Ía[0]‚ğw‚·
-
+	
 	int i;
 	for (i = 0; i < 5; i++)
-		printf("a[%d] = %p p + %d = %p\n", i, &a[i], i, p + i);
+		printf("a[%d] = %d  *(a+%d) = %d  p[%d] = %d *(a+%d) = %d\n", i, a[i], i, *(a + i), i, p[i], i, *(p + i));
+
+	for (i = 0; i < 5; i++)
+		printf("& a[%d] = %p a+%d = %p, &p[%d] = %p p+%d = %p\n", i, &a[i], i, (a + i), i, &p[i], i, (p + i));
 
 	return 0;
 
